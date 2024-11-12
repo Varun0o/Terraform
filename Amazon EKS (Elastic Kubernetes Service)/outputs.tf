9510499aed79c43b1fb1d587b093f1d4a9664118
Varun Sharma <83727416+Varun0o@users.noreply.github.com>
2024-11-12 20:23:17 +0530

    Update outputs.tf

--- Amazon EKS (Elastic Kubernetes Service)/outputs.tf
+++ Amazon EKS (Elastic Kubernetes Service)/outputs.tf
@@ -2,6 +2,3 @@ output "eks_cluster_endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
 
-output "eks_cluster_certificate_authority_data" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
-}
